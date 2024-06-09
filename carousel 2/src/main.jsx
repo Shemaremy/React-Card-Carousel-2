88ec62f12eee8deca696acd3fcdd65256f97dcf4
Shemaremy <remyshema20@gmail.com>
2024-06-09 19:47:37 +0300

    commit 007

--- carousel 2/src/main.jsx
+++ carousel 2/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MultipleItems from './App'      //First Card
-import CenterMode from './OtherCards'
+import CenterMode from './OtherCards'  //Second Card
 import './App.css'
 
 
