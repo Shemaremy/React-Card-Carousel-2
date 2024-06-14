b2b039e4d1f70bb187aef18eed624cead392b1e9
Shemaremy <remyshema20@gmail.com>
2024-06-14 15:08:32 +0300

    commit cards

--- carousel 2/src/OtherCards.tsx
+++ carousel 2/src/OtherCards.tsx
@@ -19,26 +19,28 @@ function CenterMode() {
         <div className="section-two">
           <h1>This one is also infinite but the speed of it is very favourable (OtherCards.jsx)</h1>
           <div className="slider-container-2">
-            <Slider {...settings}>
-              <div className="Trend-2">
-                  <div className="card-2">1</div>
-              </div>
-              <div className="Trend-2">
-                  <div className="card-2">2</div>
-              </div>
-              <div className="Trend-2">
-                  <div className="card-2">3</div>
-              </div>
-              <div className="Trend-2">
-                  <div className="card-2">4</div>
-              </div>
-              <div className="Trend-2">
-                  <div className="card-2">5</div>
-              </div>
-              <div className="Trend-2">
-                  <div className="card-2">6</div>
-              </div>
-            </Slider>
+            <div className="all">
+              <Slider {...settings}>
+                <div className="Trend-2">
+                    <div className="card-2">1</div>
+                </div>
+                <div className="Trend-2">
+                    <div className="card-2">2</div>
+                </div>
+                <div className="Trend-2">
+                    <div className="card-2">3</div>
+                </div>
+                <div className="Trend-2">
+                    <div className="card-2">4</div>
+                </div>
+                <div className="Trend-2">
+                    <div className="card-2">5</div>
+                </div>
+                <div className="Trend-2">
+                    <div className="card-2">6</div>
+                </div>
+              </Slider>
+            </div>
           </div>
         </div>
       </>
